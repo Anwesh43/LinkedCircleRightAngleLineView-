2343be538986a4144bfc625b7e18914d74bd8d63
Anwesh43 <anweshthecool0@gmail.com>
2020-07-14 04:19:18 +0530

    adding CircleRightAngleLineView class

--- circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
+++ circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawCRALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCircleRightAngleLines(scale, w, h, paint)
 }
+
+class CircleRightAngleLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
