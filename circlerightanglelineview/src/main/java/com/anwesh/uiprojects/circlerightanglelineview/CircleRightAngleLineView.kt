6762219783c4d2a46ef0da0d11c8896434186f3c
Anwesh43 <anweshthecool0@gmail.com>
2020-07-14 04:30:41 +0530

    adding Renderer class

--- circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
+++ circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
@@ -182,4 +182,27 @@ class CircleRightAngleLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CircleRightAngleLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val cral : CircleRightAngleLine = CircleRightAngleLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            cral.draw(canvas, paint)
+            animator.animate {
+                cral.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            cral.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
