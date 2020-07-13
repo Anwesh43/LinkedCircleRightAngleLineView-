37305d1aaf3f92df956a3488b47fd3be87d99aa5
Anwesh43 <anweshthecool0@gmail.com>
2020-07-14 04:27:14 +0530

    adding CRALNode class

--- circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
+++ circlerightanglelineview/src/main/java/com/anwesh/uiprojects/circlerightanglelineview/CircleRightAngleLineView.kt
@@ -118,4 +118,45 @@ class CircleRightAngleLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CRALNode(var i : Int, val state : State = State()) {
+
+        private var next : CRALNode? = null
+        private var prev : CRALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CRALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCRALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CRALNode {
+            var curr : CRALNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr !=  null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
